f923349740008eef36c192221d1491dea145e9b6
Matt Brindley <matt@fauxfiction.com>
2014-06-03 19:58:32 +1000

    Objectify boots, make for walking

--- CYBER.SHOES/cyber_shoes.js
+++ CYBER.SHOES/cyber_shoes.js
@@ -1,45 +1,50 @@
-walk(document.body);
+cyber = {
+    shoes: {
+        walk: function(node) {
+            // I stole this function from here:
+            // https://github.com/hank/cloud-to-butt/blob/master/
+                // He stole this function from here:
+                // http://is.gd/mwZp7E
 
-function walk(node) {
-    // I stole this function from here:
-    // https://github.com/hank/cloud-to-butt/blob/master/
-	// He stole this function from here:
-	// http://is.gd/mwZp7E
-
-    var child, next;
-    switch (node.nodeType) {
-        case 1: //Element
-        case 9: //Document
-        case 11: //Document Fragment
-            child = node.firstChild;
-            while (child) {
-                next = child.nextSibling;
-                walk(child);
-                child = next;
+            var child, next;
+            switch (node.nodeType) {
+                case 1: //Element
+                case 9: //Document
+                case 11: //Document Fragment
+                    child = node.firstChild;
+                    while (child) {
+                        next = child.nextSibling;
+                        cyber.shoes.walk(child);
+                        child = next;
+                    }
+                    break;
+                case 3: //Text node
+                    if (node.parentElement.tagName.toLowerCase() != "script") {
+                        cyber.gloves.handLeText(node);
+                    }
+                    break;
             }
-            break;
-        case 3: //Text node
-            if (node.parentElement.tagName.toLowerCase() != "script") {
-                handleText(node);
-            }
-            break;
-    }
-}
+        },
+    },
+    gloves: {
+        handLeText: function(textNode) {
+            var value = textNode.nodeValue;
+            value = value.replace(/\b(C|c)yber/g, function(match, c, offset, string) {
+                // c is either 'C' or 'c' depending on whether cyber was upper or lower case
+                    if (c === "c")
+                        return "shoe";
+                    if (c ==="C")
+                        return "Shoe";
+            });
 
-function handleText(textNode) {
-    var value = textNode.nodeValue;
-    value = value.replace(/\b(C|c)yber/g, function(match, c, offset, string) {
-        // c is either 'C' or 'c' depending on whether cyber was upper or lower case
-            if (c === "c")
-                return "shoe";
-            if (c ==="C")
-                return "Shoe"; 
-    });
+            value = value.replace(/\bCYBER/g, "SHOE");
+            value = value.replace(/\bcyborg/g, "shoeborg");
+            value = value.replace(/\bCyborg/g, "Shoeborg");
 
-    value = value.replace(/\bCYBER/g, "SHOE");
-    value = value.replace(/\bcyborg/g, "shoeborg");
-    value = value.replace(/\bCyborg/g, "Shoeborg");
+            textNode.nodeValue = value;
+        },
+    },
+};
 
-    textNode.nodeValue = value;
-}
-
+// Cyber runner
+cyber.shoes.walk(document.body);
